c9824b6c2960cf4367bb8d5aba9433268a1eaa5d
Mezni <mamezni@gmail.com>
2023-10-12 21:57:04 -0400

    Final commit 2023-10-12

--- account/service/webserver.go
+++ account/service/webserver.go
@@ -6,10 +6,8 @@ import (
 )
 
 func StartWebServer(port string) {
-
         log.Println("Starting HTTP service at " + port)
-        err := http.ListenAndServe(":" + port, nil)    // Goroutine will block here
-
+        err := http.ListenAndServe(":" + port, nil)    
         if err != nil {
                 log.Println("An error occured starting HTTP listener at port " + port)
                 log.Println("Error: " + err.Error())
