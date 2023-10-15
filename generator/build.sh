9edfeb016cbd5e0dc8abb0d20e0be507369338c6
Mezni <mamezni@gmail.com>
2023-10-14 21:12:43 -0400

    Update build file with dockerhub

--- generator/build.sh
+++ generator/build.sh
@@ -2,4 +2,7 @@ export GOOS=linux
 go build -o generator-linux-amd64
 export GOOS=darwin
 
-docker build -t dali/generator .
+docker build -t mezni/generator:latest .
+
+docker tag mezni/generator generator:latest
+docker push mezni/generator:latest
