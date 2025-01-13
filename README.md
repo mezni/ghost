59f800a2764e4cd40b94bb1185bc68b06fba7977
Mezni <mamezni@gmail.com>
2025-01-13 15:33:08 +0000

    2025-01-13/0.0.9 Work New Version

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-export VERSION=0.0.8
+export VERSION=0.0.9
 export MESSAGE="Work New Version" 
 ./commit.sh $VERSION "$MESSAGE"
--- cdr_generator/src/main.rs
+++ cdr_generator/src/main.rs
@@ -23,7 +23,7 @@ impl Cdr {
         let mut rng = rand::thread_rng();
 
         let calling_number = format!("21650{}", rng.gen_range(100000..999999));
-        let called_number = format!("216{}", rng.gen_range(100000..999999));
+        let called_number = format!("216{}", rng.gen_range(10000000..99999999));
         
         let now = Utc::now().naive_utc();
         let random_seconds_ago = rng.gen_range(0..(2 * 60 * 60));
@@ -73,6 +73,24 @@ fn write_to_csv(cdrs: &[Cdr]) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+// Write CDRs to TSV
+fn write_to_tsv(cdrs: &[Cdr]) -> Result<(), Box<dyn std::error::Error>> {
+    let dir = "OUTPUT/";
+    fs::create_dir_all(dir)?;
+
+    let file_name = dir.to_string() + &generate_file_name("TSV") + ".tsv"; 
+    let mut wtr = csv::WriterBuilder::new()
+        .delimiter(b'\t')  // Specify tab delimiter
+        .from_path(&file_name)?;
+    
+    for cdr in cdrs {
+        wtr.serialize(cdr)?;
+    }
+    
+    wtr.flush()?;
+    Ok(())
+}
+
 // Write CDRs to JSON
 fn write_to_json(cdrs: &[Cdr]) -> Result<(), Box<dyn std::error::Error>> {
     let dir = "OUTPUT/";
@@ -84,11 +102,14 @@ fn write_to_json(cdrs: &[Cdr]) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-// Main function to generate and write CDRs to CSV and JSON
+// Main function to generate and write CDRs to CSV, TSV, and JSON
 fn main() {
     let cdrs = generate_cdrs(1000);
     if let Err(e) = write_to_csv(&cdrs) {
         eprintln!("Error writing to CSV: {}", e);
+    }
+    if let Err(e) = write_to_tsv(&cdrs) {
+        eprintln!("Error writing to TSV: {}", e);
     }
     if let Err(e) = write_to_json(&cdrs) {
         eprintln!("Error writing to JSON: {}", e);
