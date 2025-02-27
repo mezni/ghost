d5cba1951c4d957fbb2b0f3fc123fc0776c5df06
Mezni <mamezni@gmail.com>
2025-02-27 19:48:52 +0000

    commit 2025/02/27

--- work/test_v01/src/main.rs
+++ work/test_v01/src/main.rs
@@ -1,3 +1,28 @@
+mod domain;
+
+use fake::Fake;
+use fake::faker::company::en::CompanyName;
+use fake::faker::internet::en::FreeEmail;
+use serde_json;
+
+use crate::domain::manufacturer::Manufacturer;
+
 fn main() {
-    println!("Hello, world!");
+    // Generate fake company name and contact email
+    let company_name: String = CompanyName().fake();
+    let contact: String = FreeEmail().fake();
+
+    // Create a new Manufacturer instance
+    let manufacturer = Manufacturer::new(company_name, Some(contact));
+
+    // Print the Manufacturer instance
+    println!("Manufacturer: {:?}", manufacturer);
+
+    // Serialize the Manufacturer instance to JSON
+    let json = serde_json::to_string_pretty(&manufacturer).unwrap();
+    println!("JSON: {}", json);
+
+    // Deserialize the JSON back to a Manufacturer instance
+    let deserialized: Manufacturer = serde_json::from_str(&json).unwrap();
+    println!("Deserialized Manufacturer: {:?}", deserialized);
 }
