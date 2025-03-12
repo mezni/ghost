34bf04a6be9173f8ee2dc89d920c2948835d71d9
Mezni <mamezni@gmail.com>
2025-03-12 18:53:15 +0000

    commit 2025/03/12

--- work-py/main.py
+++ work-py/main.py
@@ -1,53 +1,72 @@
-import sqlite3
-
-def create_database(db_name, sql_script):
-    try:
-        with sqlite3.connect(db_name) as conn:
-            cursor = conn.cursor()
-            with open(sql_script, 'r') as f:
-                sql = f.read()
-            cursor.executescript(sql)
-            conn.commit()
-        return True
-    except sqlite3.Error as e:
-        print(f"Error creating database: {e}")
-        return False
-
-
-db_name = 'test.db'
-sql_script = 'schema.sql'
-#create_database(db_name, sql_script)
-
-
 import pandas as pd
-from datetime import datetime, timedelta
+import phonenumbers 
+from phonenumbers import timezone, geocoder, carrier
+file_path = '../../data.csv'
 
-# Define the start and end dates
-start_date = datetime(2020, 1, 1)
-end_date = datetime(2025, 12, 31)
+try:
+    df_batch = pd.read_csv(file_path)
+except pd.errors.EmptyDataError:
+    print("Error: CSV file is empty.")
 
-# Create a date range
-date_range = pd.date_range(start=start_date, end=end_date)
 
-# Create a DataFrame
-df = pd.DataFrame(date_range, columns=['date_key'])
+def get_info(msisdn):
+    region=None
+    country=None
+    operator=None
+    msisdn=str(msisdn)
+    if msisdn.startswith('+'):
+        phone_number=msisdn
+    else:
+        phone_number='+'+msisdn
+    
+    
+    try:
+        parsed_number = phonenumbers.parse(phone_number, None)
+        region = geocoder.description_for_number(parsed_number, "fr")
+        operator = carrier.name_for_number(parsed_number, "fr")
+        country = phonenumbers.region_code_for_number(parsed_number)       
+    except:
+        pass
+    
+    return region,country,operator
 
-# Add additional columns
-df['year'] = df['date_key'].dt.year
-df['quarter'] = df['date_key'].dt.quarter
-df['month'] = df['date_key'].dt.month
-df['month_name'] = df['date_key'].dt.strftime('%B')
-df['day'] = df['date_key'].dt.day
-df['day_of_week'] = df['date_key'].dt.dayofweek
-df['day_name'] = df['date_key'].dt.strftime('%A')
-df['is_weekend'] = df['date_key'].dt.dayofweek.isin([5, 6])
-df['is_holiday'] = False  # You can add holiday logic here
+#region,country,operator = get_info('+21697501501')
+#print (region,country,operator)
 
-# Print the DataFrame
-print(df.head())
 
-# Save to SQLite database
-import sqlite3
-conn = sqlite3.connect('calendar_dim.db')
-df.to_sql('calendar_dim', conn, if_exists='replace', index=False)
-conn.close()
+#df = df_batch[df_batch['VLR_NUMBER'].map(lambda x: str(x).startswith('33'))]
+#print(df.head(20))
+#print(df.size)
+
+
+df_batch[['region', 'country', 'operator']] = df_batch['VLR_NUMBER'].apply(lambda x: pd.Series(get_info(x)))
+#print(df_batch.head(20))
+#print(df_batch.size)
+#df_batch.to_csv('output.csv', index=False)
+
+#df_batch=df_batch.groupby(['country','operator'])['MSISDN'].count()
+#print(df_batch.head(20))
+
+
+df=df_batch.copy()
+grouped_df = df.groupby(['operator']).size().reset_index(name='counts')
+
+# Sort the results by Category and then by Value
+sorted_df = grouped_df.sort_values(by=['counts'], ascending=False)
+print(sorted_df.head(10))
+
+
+df=df_batch.copy()
+grouped_df = df.groupby(['country']).size().reset_index(name='counts')
+
+# Sort the results by Category and then by Value
+sorted_df = grouped_df.sort_values(by=['counts'], ascending=False)
+print(sorted_df.head(10))
+
+
+df=df_batch.copy()
+grouped_df = df.groupby(['region']).size().reset_index(name='counts')
+
+# Sort the results by Category and then by Value
+sorted_df = grouped_df.sort_values(by=['counts'], ascending=False)
+print(sorted_df.head(10))
