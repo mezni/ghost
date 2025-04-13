7d60f57afd8f4779f77c2afac52423b4b8134622
Mezni <mamezni@gmail.com>
2025-04-13 10:13:40 -0400

    commit 2025/04/13

--- start.sh
+++ start.sh
@@ -2,5 +2,11 @@
 
 mkdir -p WORK/INPUT WORK/ARCHIVE WORK/REJECTED
 
-docker compose build data-generator
-docker compose up data-generator
+docker compose build postgres
+docker compose up postgres -d
+
+#docker compose build data-generator
+#docker compose up data-generator
+
+#docker compose build loader-service
+#docker compose up loader-service -d
