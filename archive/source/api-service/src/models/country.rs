e830978debd9786401218f455dadcdd8d9100fd9
Mezni <mamezni@gmail.com>
2025-05-24 08:29:14 -0400

    commit 2025/05/24

--- archive/source/api-service/src/models/country.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Country {
-    pub id: i32,
-    pub name: String,
-    pub iso: String,
-}
--- archive/source/api-service/src/routes/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod countries;
-
-use actix_web::web;
-
-pub fn init(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/countries").configure(countries::init));
-}
