0ad4efa4a9040dd37cb3d6b883ff4f7969e2c87a
Mezni <mamezni@gmail.com>
2025-05-31 09:40:34 -0400

    commit 2025/05/31

--- roam_v06/api-service/src/errors.rs
+++ roam_v06/api-service/src/errors.rs
@@ -1,8 +1,12 @@
 use config;
+use std::io;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum AppError {
     #[error("Configuration Error: {0}")]
     Configuration(#[from] config::ConfigError),
+
+    #[error("IO Error: {0}")]
+    Io(#[from] io::Error),
 }
--- roam_v06/api-service/src/main.rs
+++ roam_v06/api-service/src/main.rs
@@ -1,15 +1,34 @@
 mod errors;
 mod infra;
 
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
 use errors::AppError;
 use infra::config::load_config;
 use infra::logger::Logger;
 
-fn main() -> Result<(), AppError> {
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("OK")
+}
+
+#[tokio::main]
+async fn main() -> Result<(), AppError> {
+    // Initialize logger
     Logger::init();
     Logger::info("Starting service...");
 
+    // Load configuration
     let config = load_config()?;
+    Logger::info("Configuration loaded successfully.");
+    Logger::debug(&format!("Loaded config: {:?}", config));
 
-    Ok(())
+    let host = config.service.host.clone();
+    let port = config.service.port;
+
+    Logger::info(&format!("Starting HTTP server at http://{}:{}", host, port));
+
+    HttpServer::new(|| App::new().route("/health", web::get().to(health_check)))
+        .bind((host.as_str(), port as u16))?
+        .run()
+        .await
+        .map_err(Into::into) // convert Actix error to AppError if needed
 }
