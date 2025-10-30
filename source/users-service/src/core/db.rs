a99f39196115b7ee539b298883560d383e09f9a3
Mezni <mamezni@gmail.com>
2025-10-29 20:41:38 -0400

    commit 2025/10/29

--- source/users-service/src/core/db.rs
+++
@@ -1,7 +0,0 @@
-use sqlx::postgres::PgPool;
-use crate::core::error::Result;
-
-pub async fn create_pool(database_url: &str) -> Result<PgPool> {
-    let pool = PgPool::connect(database_url).await?;
-    Ok(pool)
-}
