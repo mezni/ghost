3ed234031b8cde6b8182462e3e4a0be1a3ec7f0e
M.Mezni <mamezni@gmail.com>
2026-08-08 20:19:47 -0400

    Cleanup

--- source/source/api-service/src/auth/password.rs
+++
@@ -1,9 +0,0 @@
-use bcrypt::{DEFAULT_COST, hash, verify};
-
-pub fn hash_password(password: &str) -> Result<String, bcrypt::BcryptError> {
-    hash(password, DEFAULT_COST)
-}
-
-pub fn verify_password(password: &str, hash: &str) -> Result<bool, bcrypt::BcryptError> {
-    verify(password, hash)
-}
